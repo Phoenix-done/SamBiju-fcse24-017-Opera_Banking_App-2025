64b0845967e6215eef044f7dc7e5fe8fa0c5ea1c
Phoenix-done <Phoenixmanone@gmail.com>
2025-10-16 07:27:36 +0000

    done

---
+++ Accounts.java
@@ -0,0 +1,16 @@
+public class Accounts {
+    String test;
+
+
+public Accounts() {
+    test="tested";
+}
+
+
+ public static void main (String [] args) {
+    Accounts acc = new Accounts();
+    System.out.println(acc.test);
+ }
+
+}
+
