a2bb32620a0a0373448fa497d81b1d613db3debb
Phoenix-done <Phoenixmanone@gmail.com>
2025-10-23 06:54:43 +0000

    Moved to MYSQL from sqlite

--- User.java
+++ User.java
@@ -1,9 +1,58 @@
-public interface User {
+public abstract User {
 
-    long getUserId();
-    String getFirstName();
-    String getLastName();
-    void accessAccount();
+    private int UserId;
+    private String FirstName;
+    private String LastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+
+    public void setUserId(int userId) {
+        this.UserId = userId;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+        
 
 
 
