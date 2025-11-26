0176e76d64ac270c40e8889ec4c60a18d0dc4881
Phoenix-done <Phoenixmanone@gmail.com>
2025-11-26 07:48:20 +0000

    trying new

--- src/model/AccountOperations.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public interface AccountOperations {
-    void deposit(double amount);
-    boolean withdraw(double amount);
-    void calculateInterest();
-    String getAccountDetails();
-}
